1bb2116258415908be743dba6794e052bfe8431f
Akram Khalifa <akram.kh.125@gmail.com>
2021-10-25 10:11:11 -0400

    created private Points and the two triangle constructors

---
+++ 01-Object/Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle{
+  private Point v1,v2,v3;
+
+  public Triangle(Point a, Point b, Point c) {
+    v1 = a;
+    v2 = b;
+    v3 = c;
+  }
+
+  public Triangle(double ax, double ay, double bx, double by,
+                  double cx, double cy) {
+    v1 = new Point(ax, ay);
+    v2 = new Point(bx, by);
+    v3 = new Point(cx, cy);
+  }
+
+}
