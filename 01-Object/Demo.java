688144be8a44ccd7e41bbcc8da8c066f7bc25606
Akram Khalifa <akram.kh.125@gmail.com>
2021-10-24 20:28:41 -0400

    first static method from point

---
+++ 01-Object/Demo.java
@@ -0,0 +1,20 @@
+public class Demo{
+  /* more methods can go here*/
+  public static double distance(pp, p) {
+    double ypart = (getY(p)*getY(p)) - (getY(pp)*getY(pp));
+    double xpart = (getX(p)*getX(p)) - (getX(pp)*getX(pp));
+    return Math.sqrt(ypart + xpart);
+  }
+
+
+  public static void main(String[]args){
+    Point P1 = new Point(1,1);
+    Point P2 = new Point(2,2);
+
+    System.out.println(  distance(P1,P2)); //call1
+    System.out.println(  Point.distance(P1,P2)); //call 2
+    System.out.println(  P1.distanceTo(P2)); //call 3
+    //This one is IMPLIED strongly. You cannot tell by the calling style alone,
+    //but you can make an educated guess.
+  }
+ }
