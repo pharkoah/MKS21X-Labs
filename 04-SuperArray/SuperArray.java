ca3ed2441d2c88d9b161d353a142735d6b7f2cb0
Akram Khalifa <akram.kh.125@gmail.com>
2021-11-09 18:29:24 -0500

    actually added toStringDebug, toString, and get

--- 04-SuperArray/SuperArray.java
+++ 04-SuperArray/SuperArray.java
@@ -13,7 +13,33 @@ public class SuperArray {
 
   public boolean add(String s) {
     data[size] = s;
+    size++;
     return true;
   }
 
+  public String toString() {
+    String res = "";
+    res += "[";
+    for (int i = 0; i < size; i++) {
+        res += data[i] + ", ";
+    }res += "]";
+    return res;
+  }
+
+  public String toStringDebug() {
+    String res = "";
+    res += "[";
+    for (int i = 0; i < data.length; i++) {
+        res += data[i] + ",";
+    }res += "]";
+    return res;
+  }
+
+  public String get(int index) {
+    if ( (index < 0 ) || (index >= size) ) {
+      return null;
+    }
+    String res = data[index].toString();
+    return res;
+  }
 }
