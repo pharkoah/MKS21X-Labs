1c02e0a335e462760c3ad07c7ea12ed6d5f63e74
Akram Khalifa <akram.kh.125@gmail.com>
2021-11-10 00:10:21 -0500

    added tests for Phase2 testing get and set also added a test for size in phase 1

--- 04-SuperArray/TesterForSA.java
+++ 04-SuperArray/TesterForSA.java
@@ -3,19 +3,51 @@ public class TesterForSA {
     SuperArray alist = new SuperArray();
     alist.add("Mario");
     //displaying elements
-    System.out.println(alist);
+    System.out.println(alist.toString());
     System.out.println(alist.toStringDebug());
 
     alist.add("Samus");
     //displaying elements
-    System.out.println(alist);
+    System.out.println(alist.toString());
     System.out.println(alist.toStringDebug());
 
     alist.add("Ash");
     alist.add("Link");
     alist.add("Kirby");
     //displaying elements
-    System.out.println(alist);
+    System.out.println(alist.toString());
     System.out.println(alist.toStringDebug());
+    System.out.println("test for size: " + alist.size());
+
+    // loops
+    SuperArray blist = alist;
+// loop 1
+    System.out.println("Tests for loop 1:");
+    for (int i = 0; i < alist.size(); i++) {
+      System.out.println("Value " + i + " for loop 1: " + alist.get(i));
+    }
+
+// loop 2
+    System.out.println("Tests for loop 2:");
+    for (int i = 0; i < alist.size(); i++) {
+      alist.set(i, "cringe");
+    }
+    for (int i = 0; i < alist.size(); i++) {
+      System.out.println("Value " + i + " for loop 2: " + alist.get(i));
+    }
+
+// loop 3
+    System.out.println("Tests for loop 3:");
+    for (int i = 0; i < blist.size(); i++) {
+      if (!(i == alist.size()-1)) {
+        blist.set(i, blist.get(i+1));
+      } else {
+        blist.set(i, "NOPE!");
+      }
+    }
+    for (int i = 0; i < blist.size(); i++) {
+      System.out.println("Value " + i + " for loop 3: " + blist.get(i));
+    }
+
   }
 }
