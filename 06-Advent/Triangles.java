ab0402941189889908dd8741cd06a31802e74ae0
Akram Khalifa <akram.kh.125@gmail.com>
2021-11-23 10:12:20 -0500

    added a long complicated way of making part 2 work

--- 06-Advent/Triangles.java
+++ 06-Advent/Triangles.java
@@ -23,5 +23,49 @@ public class Triangles {
           System.out.println("no valid file here");
       }
       System.out.println(TriC);
+
+//part 2
+      int TriC2 = 0;
+      try {
+
+          File file = new File("data.txt");
+          Scanner input = new Scanner(file);
+          while (input.hasNextLine()) {
+              String line01 = input.nextLine();
+              Scanner line02 = new Scanner(line01);
+              String line03 = input.nextLine();
+              Scanner line04 = new Scanner(line03);
+              String line05 = input.nextLine();
+              Scanner line06 = new Scanner(line05);
+              //String[] sp = line.split(" ");
+              //int a = Integer.parseInt(sp[0]);
+              int a = line02.nextInt();
+              int b = line02.nextInt();
+              int c = line02.nextInt();
+              int d = line04.nextInt();
+              int e = line04.nextInt();
+              int f = line04.nextInt();
+              int g = line06.nextInt();
+              int h = line06.nextInt();
+              int i = line06.nextInt();
+              if ( (a + d > g) && (g + d > a) && (a + g > d) ) {
+                TriC2++;
+                //System.out.println(a);
+              }
+              if ( (b + e > h) && (e + h > b) && (b + h > e) ) {
+                TriC2++;
+                //System.out.println(a);
+              }
+              if ( (c + f > i) && (i + f > c) && (c + i > f) ) {
+                TriC2++;
+                //System.out.println(a);
+              }
+          }
+          input.close();
+
+      } catch (FileNotFoundException ex) {
+          System.out.println("no valid file here");
+      }
+      System.out.println(TriC2);
   }
 }
