a385c3b15e818a200123e96d74f1fe776d4e903e
Akram Khalifa <akram.kh.125@gmail.com>
2021-11-29 09:57:29 -0500

    initial push of BinarySearch

---
+++ 07-BinarySearch/BinarySearch.java
@@ -0,0 +1,12 @@
+public class BinarySearch {
+  /*Precondition: data is sorted (increasing order)
+   *@param data: the numbers to search through
+   *@param target: the number you want to find
+   *@return the index of data that contains the target value. Return -1 if the value is not in the array.
+   *If there are multiple copies return the one you find first using
+   *(min+max)/2 method described.
+   */
+  public static int binarySearch(int[]data,int target){
+    return 0;
+  }
+}  
