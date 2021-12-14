51b3f718a55ba4e1222100f137de1086734044ea
pharkoah <akram.kh.125@gmail.com>
2021-12-14 09:45:35 -0500

    cool tester for horizontal and vertical

--- 09-WordSearch/Test.java
+++ 09-WordSearch/Test.java
@@ -2,9 +2,10 @@ import java.util.Arrays;
 public class Test{
   public static void main (String[] args) {
     WordSearch x = new WordSearch(4, 5);
-    System.out.println(x.toString());
+    x.toString();
     System.out.println(x.addWordHorizontal("woo", 3, 0));
-    System.out.println(x.toString());
-
+    x.toString();
+    System.out.println(x.addWordVertical("hiii", 0, 2));
+    x.toString();
   }
 }
