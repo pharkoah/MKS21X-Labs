04eb7c57017cde6c158916d1fa58e3669215df40
pharkoah <akram.kh.125@gmail.com>
2021-12-14 10:00:09 -0500

    cooler tester

--- 09-WordSearch/Test.java
+++ 09-WordSearch/Test.java
@@ -3,9 +3,11 @@ public class Test{
   public static void main (String[] args) {
     WordSearch x = new WordSearch(4, 5);
     x.toString();
-    System.out.println(x.addWordHorizontal("woo", 3, 0));
-    x.toString();
-    System.out.println(x.addWordVertical("hiii", 0, 2));
-    x.toString();
+    x.addWordHorizontal("woo", 3, 0);
+    x.toString(); // initial horizontal test
+    x.addWordVertical("hiii", 0, 2);
+    x.toString(); // overlap test
+    x.addWordVertical("hiii", 0, 4);
+    x.toString(); // initial vertical test
   }
 }
