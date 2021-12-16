300f7b01b1455b9f2d1ebf62f18b28768ca7b472
Akram Khalifa <akram.kh.125@gmail.com>
2021-12-16 10:13:40 -0500

    trying to understand this lab

---
+++ 11-ExtendedArrayList/NoNullArrayList.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class NoNullArrayList<T> extends ArrayList<T> {
+  private T value;
+
+  public boolean add (T e) {
+
+  }
+
+  public void add(int index, T element) {
+
+  }
+
+  public T set(int index, T element) {
+    
+  }
+
+}
