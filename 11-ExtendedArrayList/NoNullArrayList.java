b467d3d62217e01448fb9364d9f03214447c7886
Akram Khalifa <akram.kh.125@gmail.com>
2021-12-17 10:09:47 -0500

    hopefully a successful override of add

--- 11-ExtendedArrayList/NoNullArrayList.java
+++ 11-ExtendedArrayList/NoNullArrayList.java
@@ -2,16 +2,27 @@ import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
   private T value;
 
-  public boolean add (T e) {
 
-  }
+  /*public boolean add (T e) {
+    try {
+      super.add(e);
+    } catch(IllegalStateException) {
 
+    }
+  }*/
+
+  @Override
   public void add(int index, T element) {
-
+    if (element == null) {
+      throw new IllegalArgumentException ("Null can not be added");
+    } else {
+      super.add(index, element);
+    }
   }
 
+  /* @Override
   public T set(int index, T element) {
-    
-  }
+
+  }*/
 
 }
