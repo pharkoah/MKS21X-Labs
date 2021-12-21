1c7c49722039b20a78e032c4e3efc24562eb74c9
pharkoah <akram.kh.125@gmail.com>
2021-12-21 10:07:26 -0500

    added overrides for everything else

--- 11-ExtendedArrayList/NoNullArrayList.java
+++ 11-ExtendedArrayList/NoNullArrayList.java
@@ -2,16 +2,17 @@ import java.util.ArrayList;
 public class NoNullArrayList<T> extends ArrayList<T> {
   private T value;
 
-
-  /*public boolean add (T e) {
-    try {
-      super.add(e);
-    } catch(IllegalStateException) {
-
-    }
-  }*/
-
   @Override
+  public boolean add (T e) {
+    if (e == null) {
+      throw new IllegalArgumentException ("Null can not be added");
+    } else {
+      super.add(e);
+      return true;
+    }
+  }
+
+
   public void add(int index, T element) {
     if (element == null) {
       throw new IllegalArgumentException ("Null can not be added");
@@ -20,9 +21,13 @@ public class NoNullArrayList<T> extends ArrayList<T> {
     }
   }
 
-  /* @Override
-  public T set(int index, T element) {
 
-  }*/
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException ("Null can not be added");
+    } else {
+      super.set(index, element);
+    }
+  }
 
 }
