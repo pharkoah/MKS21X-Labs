bda7ecbb76db3d3c6d3bae0fa81aac4e3cb51d10
Akram Khalifa <akram.kh.125@gmail.com>
2021-12-31 16:51:30 -0500

    tester that works for no null

--- 11-ExtendedArrayList/Test.java
+++ 11-ExtendedArrayList/Test.java
@@ -4,21 +4,20 @@ public class Test{
    public static void main(String args[]){
       NoNullArrayList alist = new NoNullArrayList();
       alist.add("Steve");
-      alist.add("Tim");
-      alist.add("Lucy");
+      alist.add("0");
+      alist.add(0);
       alist.add("Pat");
       alist.add("Angela");
       alist.set(1, "Cringe");
       System.out.println(alist);
+      NoNullArrayList b = new NoNullArrayList();
+      //b.set(1, "emilay");
+      b.add("woo");
+      b.set(0, 1);
+      //b.set(3, "hi");
+      //b.set(2, "u suck");
+      System.out.println(b);
       alist.add(null);
 
-      //displaying elements
-      System.out.println(alist);
-
-      //Adding "Steve" at the fourth position
-      alist.add(3, "Steve");
-
-      //displaying elements
-      System.out.println(alist);
    }
 }
