a84af92b23f68d930cb9cfb1141f53b278af207f
Akram Khalifa <akram.kh.125@gmail.com>
2021-12-31 19:47:27 -0500

    tester that works

--- 11-ExtendedArrayList/Test.java
+++ 11-ExtendedArrayList/Test.java
@@ -13,11 +13,25 @@ public class Test{
       NoNullArrayList b = new NoNullArrayList();
       //b.set(1, "emilay");
       b.add("woo");
+      b.add("amogus");
+      b.add("fortnite");
+      b.add("cringe");
       b.set(0, 1);
-      //b.set(3, "hi");
-      //b.set(2, "u suck");
+      b.set(3, "hi");
+      b.set(2, "u suck");
       System.out.println(b);
-      alist.add(null);
+      //alist.add(null);      null was not added exception went into action
 
+      OrderedArrayList c = new OrderedArrayList(4);
+      c.add(1);
+      c.add(4);
+      System.out.println(c);
+      c.add(1, 5);
+      System.out.println(c);
+      c.add(0);
+      System.out.println(c);
+      c.add(6);
+      System.out.println(c);
+      //c.set(2, null);       null was not added exception went into action
    }
 }
