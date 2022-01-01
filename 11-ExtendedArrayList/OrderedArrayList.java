9254c61a910bc2356cf4ede05fa9285c39169207
Akram Khalifa <akram.kh.125@gmail.com>
2021-12-31 19:22:19 -0500

    add methods

--- 11-ExtendedArrayList/OrderedArrayList.java
+++ 11-ExtendedArrayList/OrderedArrayList.java
@@ -7,4 +7,28 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public OrderedArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+  @Override
+
+  public boolean add (T e) {
+    int correct = whereToPlace(e);
+    super.add(correct, e);
+    return true;
+  }
+
+
+  public void add(int index, T element) {
+    index = whereToPlace(element);
+    super.add(index, element);
+  }
+
+  private int whereToPlace(T value){
+    /*return the index that the value should be placed
+    when inserting into the OrderedArrayList .*/
+    for (int i = 0; i < size(); i++) {
+      if (value.compareTo(get(i)) <= 0) {
+        return i;
+      }
+    }
+    return -1;
+  }
 }
