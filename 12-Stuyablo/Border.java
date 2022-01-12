2c7f56747f96f73136974d2049cede26866cd856
pharkoah <akram.kh.125@gmail.com>
2022-01-12 10:15:22 -0500

    working border

--- 12-Stuyablo/Border.java
+++ 12-Stuyablo/Border.java
@@ -1,18 +1,27 @@
 public class Border{
-
-  public static String Border(int row, int col) {
+  //private String[][]bord;
+  public static void Border(int row, int col) {
     Text.clear();
     Text.go(0,0);
-    String[][] bord = new String[row][col];
+    //bord = new String[row][col];
     for (int i = 0; i < row; i++) {
-      bord[i][0] = Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD);
-      bord[i][col-1] = Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD);
+      Text.go(i, 0);
+      System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
+      Text.go(i, col-1);
+      System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
     }
 
     for (int j = 0; j < col; j++) {
-      bord[row - 1][j] = Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD);
-      bord[row - 1][j] = Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD);
+      Text.go(0,j);
+      System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
+      Text.go(row-1,j);
+      System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
     }
+    System.out.println();
   }
+  public static void main(String[] args) {
+    Border(30, 80);
+  }
+
 
 }
