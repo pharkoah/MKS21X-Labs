858b0a20faa09fc0c30994085a46ee1798e89edc
pharkoah <akram.kh.125@gmail.com>
2022-01-13 10:11:21 -0500

    working border code

--- 12-Stuyablo/Border.java
+++ 12-Stuyablo/Border.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Border{
   //private String[][]bord;
   public static void Border(int row, int col) {
@@ -7,20 +8,27 @@ public class Border{
     for (int i = 0; i < row; i++) {
       Text.go(i, 0);
       System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
-      Text.go(i, col-1);
+      Text.go(i+1, col);
       System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
     }
 
     for (int j = 0; j < col; j++) {
-      Text.go(0,j);
+      Text.go(0, j);
       System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
-      Text.go(row-1,j);
+      Text.go(row, j+1);
       System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
     }
     System.out.println();
   }
+
+  public static void rArr(int row, int col) {
+    int random = (int)(Math.random()*100);
+    //for (int i = 0; i < 5)
+    //System.out.print(Text.colorize(random, Text.BLUE, Text.BOLD));
+  }
+
   public static void main(String[] args) {
-    Border(30, 80);
+    Border(4, 5);
   }
 
 
