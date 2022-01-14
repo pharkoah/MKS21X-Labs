4b8bad4235f452734f4d99161711f249f4ec1de9
pharkoah <akram.kh.125@gmail.com>
2022-01-14 10:03:25 -0500

    workingish border

--- 12-Stuyablo/Border.java
+++ 12-Stuyablo/Border.java
@@ -18,13 +18,15 @@ public class Border{
       Text.go(row, j+1);
       System.out.print(Text.colorize("c", Text.BLUE, Text.WHITE + Text.BACKGROUND, Text.BOLD));
     }
+    rArr(row, col);
     System.out.println();
   }
 
   public static void rArr(int row, int col) {
     int random = (int)(Math.random()*100);
     int[] arr = new int[4];
-    if (random < 25) {
+
+    /*if (random < 25) {
       System.out.print(Text.colorize("c", Text.RED, Text.BOLD));
     }
     if (random > 75) {
@@ -32,11 +34,34 @@ public class Border{
     }
     else {
       System.out.print(Text.colorize("c", Text.WHITE, Text.BOLD));
+    }*/
+    Text.go(2, ((col-2)/4)-2);
+    String space = "";
+    for (int x = 0; x < ((col-2)/4)-2; x++) {
+      space += " ";
+    }
+    for (int i = 0; i < arr.length; i++) {
+      String res = "";
+      arr[i] = (int)(Math.random()*100);
+      res += arr[i];
+      if (arr[i] < 25) {
+        System.out.print(Text.colorize(res, Text.RED, Text.BOLD));
+      }
+      if (arr[i] > 75) {
+        System.out.print(Text.colorize(res, Text.BLACK, Text.BOLD));
+      }
+      else {
+        System.out.print(Text.colorize(res, Text.WHITE, Text.BOLD));
+      }
+      if (i != arr.length-1) {
+        System.out.print(space);
+      }
     }
   }
 
   public static void main(String[] args) {
-    Border(4, 5);
+    Border(30, 80);
+    //rArr(30, 80);
   }
 
 
