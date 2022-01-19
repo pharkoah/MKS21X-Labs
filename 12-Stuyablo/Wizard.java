2221ce6a8e9e0f0f79c1f310817786ac6a5d8ee7
Akram Khalifa <akram.kh.125@gmail.com>
2022-01-18 20:55:56 -0500

    made wizard.java

---
+++ 12-Stuyablo/Wizard.java
@@ -0,0 +1,66 @@
+public class Wizard extends Adventurer {
+  private int magic;
+  private int magicMax;
+
+  public Wizard() {
+     this("Muhammad Avdol");
+  }
+
+  public Wizard(String name) {
+     this(name, 45);
+  }
+
+  public Wizard(String name, int magic) {
+    this(name, magic, 30 + (int)(Math.random()*5));
+  }
+
+  public Wizard(String name, int magic, int HP) {
+    super(name, HP);
+    this.magic = magic;
+    this.magicMax = magic;
+  }
+
+  //wizard methods
+  public String attack(Damageable other) {
+    int damage = (int)(Math.random()*10)+1;
+    other.applyDamage(damage);
+    this.magic = (getSpecial() + 1);
+    return (this +
+    " attacked " + other + " for " +
+    damage + " damage!");
+  }
+
+  public String specialAttack(Damageable other) {
+    if(getSpecial() >= 10) {
+      int damage = (int)(Math.random()*20)+1;
+      other.applyDamage(damage);
+      this.magic = (getSpecial() - 10);
+      return (this + " unleashes his magic upon "
+      + other + " for " + damage + " damage! ");
+    } else {
+      attack(other);
+      return ("Not enough magic! ");
+    }
+  }
+
+  //get methods
+  public int getMagic() {
+    return magic;
+  }
+
+  //set methods
+  public void setMagic(int r) {
+    this.magic = r;
+  }
+
+  public String getSpecialName() {
+    return "Magic";
+  }
+  public int getSpecialMax() {
+    return magicMax;
+  }
+  public int getSpecial() {
+    return magic;
+  }
+
+}
