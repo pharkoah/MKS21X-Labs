608858daabc67d625fb5ef3897b2b0ebe59e79e7
Akram Khalifa <akram.kh.125@gmail.com>
2022-01-19 01:38:56 -0500

    need to work out some kinks

--- 12-Stuyablo/StuyabloGame.java
+++ 12-Stuyablo/StuyabloGame.java
@@ -12,13 +12,12 @@ public class StuyabloGame{
   //Display a List of 1-4 adventurers on the rows row through row+3 (4 rows max)
   //Should include Name and HP on 2 separate lines. (more to be added later)
   public static void drawParty(ArrayList<Adventurer> party, int startRow){
-    party = new ArrayList<Adventurer>(3);
     for (int i = 0; i < party.size(); i++) {
-      Text.go(startRow + 1, 2);
+      Text.go(startRow, 2);
       System.out.println(party.get(i));
-      Text.go(startRow + 2, 2);
+      Text.go(startRow + 1, 2);
       System.out.println(party.get(i).getSpecialName() + ": " + party.get(i).getSpecial() + "/" + party.get(i).getSpecialMax());
-      Text.go(startRow + 3, 2);
+      Text.go(startRow + 2, 2);
       System.out.println();
     }
   }
@@ -76,22 +75,21 @@ public class StuyabloGame{
     //Things to attack:
     //Make an ArrayList of Adventurers and add 1 enemy to it.
     ArrayList<Adventurer>enemies = new ArrayList<>();
-    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-    //YOUR CODE HERE
-    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    enemies.add(new EnemyStand("DIO", 54, 150, "ZA WARUDO"));
 
     //Adventurers you control:
     //Make an ArrayList of Adventurers and add 3 Adventurers to it.
     ArrayList<Adventurer> party = new ArrayList<>();
-    /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-    //YOUR CODE HERE
-    /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+    party.add(new Warrior("Geralt"));
+    party.add(new EnemyStand("JOTARO", 45, 102, "STAR PLATINUM"));
+    party.add(new Wizard("Edward"));
 
     //Main loop
     boolean partyTurn = false;
     int whichPlayer = 0;
     int turn = 0;
-
+    int ranEnemies = (int)(Math.random() * enemies.size());
+    int ranPlayers = (int)(Math.random() * party.size());
 
     String input = "";
     Scanner in = new Scanner(System.in);
@@ -104,15 +102,11 @@ public class StuyabloGame{
       //display event based on last turn's input
       if(partyTurn){
         //Process user input:
-        if(input.equals("attack")){
-          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-          //YOUR CODE HERE
-          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+        if(input.equals("attack") || input.equals("")){
+          drawText(party.get(whichPlayer).attack(enemies.get(ranEnemies)), 12);
         }
         else if(input.equals("special")){
-          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-          //YOUR CODE HERE
-          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+          drawText(party.get(whichPlayer).specialAttack(enemies.get(ranEnemies)), 12);
         }
         whichPlayer++;
 
@@ -125,21 +119,23 @@ public class StuyabloGame{
           drawText("press enter to see monster's turn",HEIGHT/2);
           partyTurn = false;
         }
-      }else{
+      }else {
         //this block ignores user input!
         //display enemy attack except on turn 0.
-        if(turn > 0){
-          //Enemy action choices go here!
-          /*>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>*/
-          //YOUR CODE HERE
-          /*<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<*/
+        if (turn > 0){
+          int ranAttack = (int)(Math.random()*10);
+          if (ranAttack == 9) {
+            drawText(enemies.get(0).specialAttack(enemies.get(ranPlayers)), 12);
+          } else {
+            drawText(enemies.get(0).attack(enemies.get(ranPlayers)), 12);
+          }
         }
 
         //after enemy goes, change back to player's turn.
         partyTurn=true;
         whichPlayer = 0;
         //display which player's turn is next and prompt for action.
-        drawText("Enter command for "+party.get(whichPlayer)+": attack/special/quit",HEIGHT/2);
+        drawText("Enter command for " + party.get(whichPlayer )+ ": attack/special/quit", HEIGHT/2);
 
         //end the turn.
         turn++;
